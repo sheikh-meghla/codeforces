ba0dc0489f5e5ce4686847ae90ccdd509ee29ce0
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-05 22:51:42 +0600

    Add implementation for "A. Domino Piling" problem

---
+++ A_Domino_piling.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int a,b;
+    cin >> a >> b;
+    int mul = a*b;
+    int ans = mul / 2;
+    cout << ans<<endl;
+    return 0;
+}
