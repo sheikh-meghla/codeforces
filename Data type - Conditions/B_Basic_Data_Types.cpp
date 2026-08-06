2cb99fa1a57db82fecee676db44e55d30ebe941b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-06 09:43:54 +0600

    Add implementation for "B. Basic Data Types" problem

---
+++ Data type - Conditions/B_Basic_Data_Types.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a;
+    long long b;
+    char ch;
+    float f;
+    double lf;
+
+    cin >> a >> b >> ch >> f >> lf;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << ch << endl;
+    cout << f << endl;
+    cout << fixed << setprecision(1) << lf << endl;
+
+    return 0;
+}
