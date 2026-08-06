3a158397beae89fa3889cad78a57d43d88db9db9
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-06 20:06:35 +0600

    Add implementation for "A. 1 to N" problem and setup VSCode configuration files

---
+++ loops/A_1_to_N.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+
+    ll n;
+    cin >> n;
+    for (ll i = 1; i <= n; i++)
+    {
+        cout << i <<"\n";
+    }
+    
+    
+    return 0;
+}
