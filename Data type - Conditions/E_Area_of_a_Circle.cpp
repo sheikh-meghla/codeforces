47964796d710be7ea616bdffe3d42ba63ce08031
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-06 10:01:14 +0600

    Add implementation for "E. Area of a Circle" problem

---
+++ Data type - Conditions/E_Area_of_a_Circle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+using lf = double;
+int main() {
+    lf pi = 3.141592653;
+    lf r;
+    cin >>r;
+
+    lf area = pi * r * r;
+    cout << fixed << setprecision(9) << area << endl;
+    return 0;
+}
