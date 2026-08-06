3d2cc06c7dffa82eff37b5daa33e7565de291236
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-06 16:23:40 +0600

    Add implementation for "U. Float or int" problem

---
+++ Data type - Conditions/U_Float_or_int.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    char c;
+    int n2;
+    cin >> n >>c>>n2;
+    if (n2 == 0)
+    {
+        cout << "int "<<n<<endl;
+    }
+    else{
+        cout << "float "<<n<<" "<< 0<<c<<n2<<endl;
+    }
+    return 0;
+}
