08bbfccd43a0efc9d0cc86492d7981307782697b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-06 12:48:37 +0600

    Add implementation for "P. First digit !" problem

---
+++ Data type - Conditions/P_First_digit.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    string x;
+    cin >> x;
+    if (x[0]%2==0)
+    {
+        cout<<"EVEN\n";
+    }
+    else{
+        cout << "ODD\n";
+    }
+    
+    return 0;
+}
