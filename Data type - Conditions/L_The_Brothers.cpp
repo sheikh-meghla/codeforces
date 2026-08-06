fd97270a19001c6291c879dc3869d548f790721e
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-06 12:25:23 +0600

    Add implementation for "L. The Brothers" problem

---
+++ Data type - Conditions/L_The_Brothers.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    string a,b;
+    cin >> a>>b;
+    string a2,b2;
+    cin >>a2>>b2;
+    if (b == b2)
+    {
+        cout << "ARE Brothers\n";
+    }
+    else{
+        cout << "NOT\n";
+
+    }
+    
+
+    return 0;
+}
