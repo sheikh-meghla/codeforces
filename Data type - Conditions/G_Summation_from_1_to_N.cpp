6ff9773af95b3c1a60c559dac589797618085c5b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-06 10:31:32 +0600

    Add implementation for "G. Summation from 1 to N" problem

---
+++ Data type - Conditions/G_Summation_from_1_to_N.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+int main() {
+
+    ll n;
+    cin >> n;
+    ll sum = (n * (n+1))/2;
+    cout << sum <<endl;
+    return 0;
+}
