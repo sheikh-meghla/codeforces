c4ceb6a2df93703223adecdf3aad1c787f351b75
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-06 09:48:11 +0600

    Add implementation for "C. Simple Calculator" problem

---
+++ Data type - Conditions/C_Simple_Calculator.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+int main() {
+
+    ll a,b;
+    cin >> a>>b;
+    cout << a << " + "<<b<<" = "<<a+b<<endl;
+    cout << a << " * "<<b<<" = "<<a*b<<endl;
+    cout << a << " - "<<b<<" = "<<a-b<<endl;
+    return 0;
+}
