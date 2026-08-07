cca629cbc460a7ef59e910e25cd84cc35919aa86
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-07 14:12:15 +0600

    Add implementation for "N. Numbers Histogram" problem

---
+++ loops/N_Numbers_Histogram.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char s;
+    cin >> s;
+
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int x;
+        cin >> x;
+
+        while (x--) {
+            cout << s;
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
