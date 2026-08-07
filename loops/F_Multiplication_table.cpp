bf15fee43ae1a4c0fe88ceebafaea8b854032595
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-07 08:31:44 +0600

    Add implementation for "F. Multiplication table" problem

---
+++ loops/F_Multiplication_table.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    for(int i = 1; i <= 12; i++) {
+        int ans = n * i;
+        cout << n <<" " <<"*" <<" " <<i<<" = "<< ans <<"\n";
+    }
+    return 0;
+}
