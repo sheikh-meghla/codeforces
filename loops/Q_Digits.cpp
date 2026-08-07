c83143e8d737b4fc5737345f1f3663a41a96e258
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-07 14:23:28 +0600

    Add implementation for "Q. Digits" problem

---
+++ loops/Q_Digits.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n == 0) {
+            cout << 0 << '\n';
+            continue;
+        }
+
+        while (n > 0) {
+            cout << n % 10 << " ";
+            n /= 10;
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
