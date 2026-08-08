238a722879f313ed239c78902b34fb9e619db7fe
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-08 11:39:31 +0600

    Add implementation for "G. Palindrome Array" problem

---
+++ array/G_Palindrome_Array.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    bool flag = true;
+    int l = 0;
+    int r = n-1;
+    while(l < r) {
+        if(a[l] != a[r]) {
+            flag = false;
+            break;
+        }
+
+        l++;
+        r--;
+    }
+
+    if(flag) {
+        cout << "YES\n";
+    }
+    else {
+        cout << "NO\n";
+    }
+    return 0;
+}
