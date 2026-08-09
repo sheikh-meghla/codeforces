5eee2f9a6a6e92124014f61c5f14f204d7ff74b8
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-09 16:05:19 +0600

    Add implementation for "O. Fibonacci" problem

---
+++ O_Fibonacci.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    long long a = 0;
+    long long b = 1;
+
+    if (n == 1) {
+        cout << 0 << endl;
+    }
+    else if (n == 2) {
+        cout << 1 << endl;
+    }
+    else {
+        for (int i = 3; i <= n; i++) {
+            long long c = a + b;
+            a = b;
+            b = c;
+        }
+
+        cout << b << endl;
+    }
+
+    return 0;
+}
