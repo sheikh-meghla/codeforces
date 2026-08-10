6e15f898a26afe8d69d2cb1479441959cd828965
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-10 15:29:03 +0600

    Add implementation for "C. Compare" problem

---
+++ string/C_Compare.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a, b;
+
+    cin >> a >> b;
+
+    if (a < b)
+    {
+        cout << a;
+    }
+    else
+    {
+        cout << b;
+    }
+
+    return 0;
+}
