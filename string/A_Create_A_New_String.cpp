ecc1f94f1d6897ce446224b08641aa5c108e07ce
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-10 15:22:14 +0600

    Add implementation for "A. Create A New String" problem

---
+++ string/A_Create_A_New_String.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    string a,b;
+    cin >> a >> b;
+    string c = a + " "+b;
+    cout <<a.size()<<" "<<b.size()<<endl<< c <<endl;
+    return 0;
+}
