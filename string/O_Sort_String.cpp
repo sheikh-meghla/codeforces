5aa1d575884e992fffe3f56f626c00c094b75d54
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-10 20:14:46 +0600

    Add implementation for "O. Sort String" problem

---
+++ string/O_Sort_String.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[26] = {};
+
+    for (int i = 0; i < n; i++) {
+        char c;
+        cin >> c;
+
+        arr[c - 'a']++;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < arr[i]; j++) {
+            cout << char(i + 'a');
+        }
+    }
+
+    return 0;
+}
