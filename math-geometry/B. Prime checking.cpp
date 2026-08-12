df5008e47134d823f6c99e055bee391e8034bcfa
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-12 15:39:48 +0600

    Add implementation for "B. Prime checking" problem

---
+++ math-geometry/B. Prime checking.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    if (n < 2)
+    {
+        cout << "NO\n";
+        return 0;
+    }
+    for (long long i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "NO\n";
+            return 0;
+        }
+    }
+
+    cout << "YES\n";
+
+    return 0;
+}
