8c6fcceed87be49e42af38338f31eb5dd57a219d
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-12 16:12:45 +0600

    Add implementation for "D. Xor" problem

---
+++ math-geometry/D.Xor.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a, b, q;
+    cin >> a >> b >> q;
+
+    if (q % 3 == 1)
+    {
+        cout << a;
+    }
+    else if (q % 3 == 2)
+    {
+        cout << b;
+    }
+    else
+    {
+        cout << (a ^ b);
+    }
+
+    return 0;
+}
