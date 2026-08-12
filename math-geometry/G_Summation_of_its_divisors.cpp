92e50ae07c82fa4bb12527892100b313617b82d4
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-12 16:58:38 +0600

    Add implementation for "G. Summation of its divisors" problem Update test cases for "F. Multiplication of Matrices" problem

---
+++ math-geometry/G_Summation_of_its_divisors.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    long long sum = 0;
+
+    for (long long i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            sum += i;
+
+            if (i != n / i)
+            {
+                sum += n / i;
+            }
+        }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
