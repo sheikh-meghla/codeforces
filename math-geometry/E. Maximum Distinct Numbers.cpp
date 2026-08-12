7fb1dfe861af8b268567bb82aacf2e724d18e625
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-12 16:24:15 +0600

    Add implementation for "E. Maximum Distinct Numbers" problem

---
+++ math-geometry/E. Maximum Distinct Numbers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    long long k = (-1 + sqrt(1 + 8.0 * n)) / 2;
+
+    cout << k;
+
+    return 0;
+}
