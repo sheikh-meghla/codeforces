73897c0cca4529da7fca5b2a9e6b62c536913d4e
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-13 15:25:04 +0600

    Add implementation for "G. Pyramid" problem

---
+++ Recursion/G_Pyramid.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pyramid(int row, int n) {
+    if (row > n) {
+        return;
+    }
+
+    for (int i = 0; i < n - row; i++) {
+        cout << " ";
+    }
+    for (int i = 0; i < 2 * row - 1; i++) {
+        cout << "*";
+    }
+    cout << '\n';
+    pyramid(row + 1, n);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    pyramid(1, n);
+
+    return 0;
+}
