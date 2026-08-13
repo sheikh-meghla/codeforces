e0be7f5fda6c10c264952dbe08673bc6f6ac5605
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-13 14:23:51 +0600

    Refactor output formatting in "C. Print from N to 1" problem implementation

--- Recursion/C_Print_from_N_to_1.cpp
+++ Recursion/C_Print_from_N_to_1.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 
 void n_to_one(int n) {
-    
-    if(n==0){
+    if(n == 0) {
         return;
     }
-    cout << n <<" ";
-    n_to_one(n-1);
-    
 
+    cout << n;
+
+    if(n != 1) {
+        cout << " ";
+    }
+
+    n_to_one(n - 1);
 }
-int main(){
+
+int main() {
     int n;
     cin >> n;
+
     n_to_one(n);
 
     return 0;
