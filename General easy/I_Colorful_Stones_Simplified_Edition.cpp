9c52a9c34857e8b0336f48b7084a7f8d024ceb11
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-14 15:18:09 +0600

    Add implementation for "I. Colorful Stones (Simplified Edition)" problem

---
+++ General easy/I_Colorful_Stones_Simplified_Edition.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+
+    int pos = 0;
+    for (int i = 0; i < t.size(); i++)
+    {
+        if (s[pos] == t[i])
+        {
+            pos++;
+        }
+    }
+    cout << pos + 1 << endl;
+
+    return 0;
+}
