28a0e6c12d7fd105a2a707035859180d5757bec4
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-17 16:56:21 +0600

    Add implementation for "T. Olesya and Rodion" problem

---
+++ General medium/T_Olesya_and_Rodion.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, t;
+    cin >> n >> t;
+
+    if (n == 1 && t == 10)
+    {
+        cout << -1;
+    }
+    else if (t == 10)
+    {
+        cout << 1;
+        for (int i = 1; i < n; i++)
+        {
+            cout << 0;
+        }
+    }
+    else
+    {
+        cout << t;
+        for (int i = 1; i < n; i++)
+        {
+            cout << 0;
+        }
+    }
+
+    return 0;
+}
