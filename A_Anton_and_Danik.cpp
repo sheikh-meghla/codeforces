52c25ba742cd7d8d8b4bbcf5aa42ee669fde4639
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-19 22:31:28 +0600

    Add implementations for "A. Anton and Danik" and "A. Nearly Lucky Number" problems

---
+++ A_Anton_and_Danik.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int a = 0;
+    int d = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        if (s[i] == 'A') {
+            a++;
+        }
+        else {
+            d++;
+        }
+    }
+
+    if (a > d) 
+    {
+        cout << "Anton";
+    
+    }
+    else if (d > a) 
+    {
+        cout << "Danik";
+    }
+    else {
+        cout << "Friendship";
+    }
+
+    return 0;
+}
---
+++ A_Nearly_Lucky_Number.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string n;
+    cin >> n;
+
+    int count = 0;
+
+    for (int i = 0; i < n.size(); i++) {
+
+        if (n[i] == '4' || n[i] == '7') {
+            count++;
+        }
+    }
+
+    if (count == 4 || count == 7) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+
+    return 0;
+}
