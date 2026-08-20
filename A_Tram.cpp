d84372dda8ce135b699f0d90f954ce9d2f9acc0b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-20 21:28:27 +0600

    Add implementation for "A. Tram" problem

---
+++ A_Tram.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int current = 0;
+    int mx = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        int a, b;
+        cin >> a >> b;
+
+        current -= a;
+        current += b;
+
+        mx = max(mx, current);
+    }
+
+    cout << mx << '\n';
+
+    return 0;
+}
