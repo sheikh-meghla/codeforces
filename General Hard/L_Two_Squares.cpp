b533867435043f4c00f7f393a91ac5eac3e939f3
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-22 21:45:19 +0600

    Add implementation for "L. Two Squares" problem

---
+++ General Hard/L_Two_Squares.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    double L, S1, S2;
+    if (!(cin >> L >> S1 >> S2)) 
+    {
+        return 0;
+    }
+    int t;
+    cin >> t;
+
+    double speed_diff = abs(S1 - S2);
+
+    while (t--) {
+        double q;
+        cin >> q;
+
+        double ans = (L - sqrt(q)) * sqrt(2) / speed_diff;
+
+        cout << fixed << setprecision(6) << ans << "\n";
+    }
+
+    return 0;
+}
