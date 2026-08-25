bc4b061d166cda78b8ebe0f48b10f0873099c4fe
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-25 22:24:19 +0600

    Add implementation for "A. Candies and Two Sisters" problem

---
+++ A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        cin >> n;
+
+        if (n < 3) {
+            cout << 0 << endl;
+        }
+        else if (n % 2 == 1) {
+            cout << n / 2 << endl;
+        }
+        else {
+            cout << n / 2 - 1 << endl;
+        }
+    }
+
+    return 0;
+}
