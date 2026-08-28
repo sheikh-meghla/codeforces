ebac746266205a90ee9a989ece6fe159496b48ba
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-28 22:44:17 +0600

    Add implementation for "A. Lucky?" problem

---
+++ A_Lucky.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        int sum1 = 0, sum2 = 0;
+
+        for (int i = 0; i < 3; i++) {
+            sum1 += s[i] - '0';
+        }
+
+        for (int i = 3; i < 6; i++) {
+            sum2 += s[i] - '0';
+        }
+
+        if (sum1 == sum2)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
