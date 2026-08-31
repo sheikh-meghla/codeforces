d742a3a91549e4d39ff1050b0e4af551d13c624e
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-31 21:36:06 +0600

    Add implementation for "A. How Much Does Daytona Cost?" problem

---
+++ A_How_Much_Does_Daytona_Cost.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        bool flag = false;
+
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+
+            if (x == k) {
+                flag = true;
+            }
+        }
+
+        if (flag)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
