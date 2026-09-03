2da03d4ef147a42a7711bb5dfee257f58abcae83
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-03 17:08:17 +0600

    Add implementation for "A. Remove Smallest" problem

---
+++ A_Remove_Smallest.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        sort(a.begin(), a.end());
+
+        bool flag = true;
+
+        for (int i = 1; i < n; i++) {
+            if (a[i] - a[i - 1] > 1) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
