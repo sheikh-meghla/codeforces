6955bfa1253b6b8fd1c9d180bc74c6408bc80c77
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-08 19:52:02 +0600

    Add implementation for "B. Borze" problem

---
+++ B_Borze.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.size(); i++) {
+
+        if (s[i] == '.') {
+            cout << 0;
+        }
+        else {
+            if (s[i + 1] == '.') {
+                cout << 1;
+            }
+            else {
+                cout << 2;
+            }
+            i++;
+        }
+    }
+
+    return 0;
+}
