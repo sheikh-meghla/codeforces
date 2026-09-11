206c4ed9788c575c127af9758fb1c13d19df2d08
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-11 18:33:33 +0600

    Add implementation for "A. Creating Words" problem

---
+++ A_Creating_Words.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        char a,b,c;
+        cin >> a >> b >> c;
+        char a1,b1,c1;
+        cin >> a1 >> b1 >> c1;
+
+        
+        cout << a1 << b << c << " " << a << b1 << c1 << endl;
+
+    }
+    
+    return 0;
+}
