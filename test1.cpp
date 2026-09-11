d4e9d5f9bca637b60a8b3162268f4523274703ba
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-11 12:37:35 +0600

    Refactor code structure for improved readability and maintainability

---
+++ test1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x;
+    cin >> x;
+
+    cout << x * 2 << "\n";
+
+    return 0;
+}
