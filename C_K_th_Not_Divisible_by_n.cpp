0a422bf317b76a032c0f1a26ed771b48d5791b9d
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-11 18:54:48 +0600

    Add implementation for "C. K-th Not Divisible by n" problem

---
+++ C_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n, k;
+        cin >> n >> k;
+
+        long long ans = k + (k - 1) / (n - 1);
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
