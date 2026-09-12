ada9f9ef20162d41948bab0ae87cbb0e7de5a9da
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-12 13:09:42 +0600

    Add implementation for "A. Choosing Teams" problem

---
+++ A_Choosing_Teams.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int y;
+        cin >> y;
+
+        if (y + k <= 5)
+        {
+            count++;
+        }
+    }
+
+    cout << count / 3;
+
+    return 0;
+}
