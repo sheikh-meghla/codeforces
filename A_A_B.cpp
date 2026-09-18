073cb1dfe4ef3e4d7bd2b8dfbfa19ec18ac6b752
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-18 09:39:58 +0600

    Add implementation for "A. A+B?" problem

---
+++ A_A_B.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a,c;
+        char b;
+        cin >> a >> b >> c;
+        cout << a + c << endl;
+    }
+    
+
+    return 0;
+}
