dd7ee292cc81c3681f4e5948b190230f9ee23580
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-19 07:45:16 +0600

    Add implementation for "A. Polycarp and Coins" problem

---
+++ A_Polycarp_and_Coins.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        cin >> n;
+
+        long long c1 = n / 3;
+        long long c2 = n / 3;
+
+        if (n % 3 == 1) {
+            c1++;
+        }
+        else if (n % 3 == 2) {
+            c2++;
+        }
+
+        cout << c1 << " " << c2 << '\n';
+    }
+
+    return 0;
+}
