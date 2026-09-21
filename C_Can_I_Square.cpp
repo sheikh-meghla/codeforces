ef5e8283d8495b9347fca942832d59458bb75bd6
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-21 07:03:08 +0600

    Add implementation for "C. Can I Square?" problem

---
+++ C_Can_I_Square.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        long long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            long long x;
+            cin >> x;
+            sum += x;
+        }
+
+        long long r = sqrt(sum);
+
+        if (r * r == sum)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
