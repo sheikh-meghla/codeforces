78104ccd2350164659b14c230c76e9e4897b954e
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-21 09:02:28 +0600

    Add implementation for "B. Honest Coach" problem

---
+++ B_Honest_Coach.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        sort(a, a + n);
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i < n - 1; i++) {
+            int diff = a[i + 1] - a[i];
+
+            ans = min(ans, diff);
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
