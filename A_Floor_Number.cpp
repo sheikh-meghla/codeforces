18aec61a095b6bc1c1670c3b9f7f6a173e1f919d
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-21 07:09:46 +0600

    Add implementation for "A. Floor Number" problem

---
+++ A_Floor_Number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+
+        if (n <= 2) 
+        {
+            cout << 1 << endl;
+        } 
+        else 
+        {
+            cout << (n - 2 + x - 1) / x + 1 << endl;
+        }
+    }
+
+    return 0;
+}
