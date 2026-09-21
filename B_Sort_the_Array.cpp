0faec6cbb0e1ca533fca1829111df51cca0ab0ea
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-21 12:02:08 +0600

    Add implementation for "B. Sort the Array" problem

---
+++ B_Sort_the_Array.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<int> b = a;
+
+    sort(b.begin(), b.end());
+    int l = 0;
+    while (l < n && a[l] == b[l]) {
+        l++;
+    }
+
+    if (l == n) {
+        cout << "yes\n";
+        cout << "1 1\n";
+        return 0;
+    }
+
+    int r = n - 1;
+    while (r >= 0 && a[r] == b[r]) {
+        r--;
+    }
+    
+    reverse(a.begin() + l, a.begin() + r + 1);
+
+    if (a == b) {
+        cout << "yes\n";
+        cout << l + 1 << " " << r + 1 << "\n";
+    } else {
+        cout << "no\n";
+    }
+
+    return 0;
+}
