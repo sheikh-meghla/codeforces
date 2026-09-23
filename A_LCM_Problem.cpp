f4bf27e2f2075a1f28de5b7e80a5f0849f42d9ce
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-23 07:26:05 +0600

    Add implementation for "A. LCM Problem"

---
+++ A_LCM_Problem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long l, r;
+        cin >> l >> r;
+
+        if (2 * l <= r) 
+        {
+            cout << l << " " << 2 * l << endl;
+        } 
+        else {
+            cout << -1 << " " << -1 << endl;
+        }
+    }
+
+    return 0;
+}
