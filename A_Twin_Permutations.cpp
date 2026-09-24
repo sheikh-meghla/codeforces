47e70d5639deaff602d653ca2758bf9cbe6029eb
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-24 18:47:35 +0600

    Add implementation for "A. Twin Permutations" problem

---
+++ A_Twin_Permutations.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        for (int i = 0; i < n; i++) {
+            int a;
+            cin >> a;
+
+            cout << n + 1 - a << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
