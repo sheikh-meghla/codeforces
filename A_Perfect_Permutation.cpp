02b71c8e8ae8ea5714cf5a19d8a4e07d69518019
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-25 19:51:16 +0600

    Add implementation for "A. Perfect Permutation" problem

---
+++ A_Perfect_Permutation.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    if (n % 2 == 1) {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    for(int i = 1; i <= n; i += 2) {
+        cout << i + 1 << " " << i << " ";
+    }
+
+    return 0;
+}
