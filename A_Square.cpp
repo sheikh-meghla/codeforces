9a0d4dfe07fc0237e99e3c3580022a324dc2608a
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-25 18:11:10 +0600

    Add implementation for "A. Square" problem

--- A_Square.cpp
+++ A_Square.cpp
@@ -1,24 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int t;
     cin >> t;
 
-    while (t--)
-    {
-        int a,b,c,d;
-        cin >> a >> b >> c >> d;
-        if (a == b && a == c && a == d)
-        {
-            cout << "YES\n";
+    while (t--) {
+        int mn = INT_MAX;
+        int mx = INT_MIN;
+
+        for (int i = 0; i < 4; i++) {
+            int x, y;
+            cin >> x >> y;
+
+            mn = min(mn, x);
+            mx = max(mx, x);
         }
-        else
-        {
-            cout << "NO\n";
-        }
-        
+
+        int ans = mx - mn;
+
+        cout << ans * ans << endl;
     }
-    
+
     return 0;
 }
